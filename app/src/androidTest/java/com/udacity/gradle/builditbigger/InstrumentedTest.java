7c6ded18ea78618e53b4667034d68380671b1e44
Weblen Fernandes Pimentel <>
2019-05-05 23:47:44 -0300

    All done!

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/InstrumentedTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/InstrumentedTest.java
@@ -3,13 +3,10 @@ package com.udacity.gradle.builditbigger;
 import android.text.TextUtils;
 
 import com.udacity.gradle.builditbigger.api.JokesAsyncTask;
-import com.udacity.gradle.builditbigger.api.JokesAsyncTaskGCE;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-
-import java.util.concurrent.CountDownLatch;
 
 import static junit.framework.Assert.assertFalse;
 
@@ -24,14 +21,6 @@ public class InstrumentedTest {
     @Test
     public void testFetchJokesFromLibrary() throws Exception {
         String joke = new JokesAsyncTask().execute().get();
-        assertFalse("Error: Fetched Joke = " + joke, TextUtils.equals(joke, ERROR_MESSAGE));
-    }
-
-    @Test
-    public void testFetchJokesFromGCE() throws Exception {
-        final CountDownLatch signal = new CountDownLatch(1);
-        String               joke   = new JokesAsyncTaskGCE(signal).execute().get();
-        signal.await();
         assertFalse("Error: Fetched Joke = " + joke, TextUtils.equals(joke, ERROR_MESSAGE));
     }
 }
